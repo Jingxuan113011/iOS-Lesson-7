9330969ffec1377a731dd08c88726994756de15c
Jingxuan113011 <xie_jingxuan@s2024.ssts.edu.sg>
2025-08-25 16:50:59 +0800

    Initial Commit

---
+++ iOS Lesson 7/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS Lesson 7
+//
+//  Created by Xie jingxuan on 25/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS Lesson 7/iOS_Lesson_7App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Lesson_7App.swift
+//  iOS Lesson 7
+//
+//  Created by Xie jingxuan on 25/8/25.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Lesson_7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
